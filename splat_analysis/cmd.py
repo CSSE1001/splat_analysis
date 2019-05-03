6e91ba8680aba35ab32d7c3525ae8322154ac6b5
Rudi Scarpa <rudi.scarpa@student.uq.edu.au>
2019-05-03 13:16:58 +1000

    Changed CLI to echo out appropriate JSON data as opposed to direct file save

--- splat_analysis/cmd.py
+++ splat_analysis/cmd.py
@@ -6,27 +6,22 @@ from pathlib import Path
 
 SUBMISSION_FILES = ('event_decision.py', 'prediction.py')
 
-RESULT_PATHS = (
-    ('get_static_results', 'static_results.json'),
-    ('get_static_messages', 'static_messages.json'),
-    ('get_rubric_data', 'suggested_programming_constructs.json')
-)
-
-# usage: splat_analysis path_to_submission
-
-# will save static_results.json, static_messages.json and
-# suggested_programming_constructs.json
+ARG_OPTIONS = {
+    '-static': 'get_static_results',
+    '-messages': 'get_static_messages',
+    '-rubric': 'get_rubric_data'
+}
 
 def splat_analysis_cli():
     """
-    usage: splat_analysis path_to_submission
+    usage: splat_analysis path_to_submission (-static | -messages | -rubric)
 
     will save static_results.json, static_messages.json and
     suggested_programming_constructs.json to submission_dir
     """
     args = sys.argv[1:]
-    if len(args) != 1:
-        print('Incorrect args - usage: splat_analysis path_to_submission_dir')
+    if len(args) != 2:
+        print('Incorrect args - usage: splat_analysis path_to_submission_dir (-static | -messages | -rubric)')
         return 1
 
     sources = {}
@@ -34,25 +29,22 @@ def splat_analysis_cli():
     submission_dir_path = Path(args[0])
     files = [(name, (submission_dir_path / name)) for name in SUBMISSION_FILES]
     
-    for sub_file_name, file_path in files:
+    #read in files into sources
+    for file_name, file_path in files:
         try:
-            with open(files[sub_file_name]) as sub_file:
-                sources[sub_file_name] = sub_file.read()
+            with open(file_path) as sub_file:
+                sources[file_name] = sub_file.read()
         except IOError:
-            print('Error trying to read file at address:', 
-                files[sub_file_name])
+            print('Error trying to read file at address:', file_path)
             return 2
 
     analyser = Analyser(sources)
     analyser.analyse()
 
-    for method, file_path in RESULT_PATHS:
-        try:
-            with open((submission_dir_path / file_path), 'w') as result_file:
-                data = getattr(analyser, method)()
-                json.dump(data, result_file)
-        except:
-                print('Error trying to write results to:', result_file)
+    #echo out appropriate json based on cli arg
+    method = ARG_OPTIONS[args[1]]
+    data = getattr(analyser, method)()
+    print(json.dumps(data))
     return 0
     
 
